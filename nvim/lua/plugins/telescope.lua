9e37182ef1213d5f317e33af9196f2f2224e93dd
Harri Kuittinen <harri.kuittinen@gmail.com>
2024-03-03 17:25:00 +0200

    Add hidden to telescope grep

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -31,6 +31,12 @@ return {
                 find_files = {
                     hidden = true,
                 },
+                grep_string = {
+                    additional_args = { "--hidden" },
+                },
+                live_grep = {
+                    additional_args = { "--hidden" },
+                },
             },
             extensions = {
                 ["ui-select"] = {
