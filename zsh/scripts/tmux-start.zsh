7a6c08659cb639141b010286e37648ebee0e7573
Harri Kuittinen <harri.kuittinen@gmail.com>
2024-03-16 00:57:17 +0200

    Edit tmux-start

--- zsh/scripts/tmux-start.zsh
+++ zsh/scripts/tmux-start.zsh
@@ -4,14 +4,10 @@ if [[ -v TMUX ]]; then
     exit 0
 fi
 
-if tmux has-session; then
-    echo "tmux server is already running."
-    exit 0
+if ! tmux has-session; then
+    tmux new-session -d -s dotfiles -c ~/Projects/dotfiles
+    tmux new-session -d -s notes -c ~/Notes
 fi
-
-tmux new-session -d -s dotfiles -c ~/Projects/dotfiles
-tmux new-session -d -s notes -c ~/Notes
-tmux new-session -d -s terminal -c ~
 
 if [[ $# -gt 0 ]]; then
     local project_name=$1
@@ -19,22 +15,24 @@ if [[ $# -gt 0 ]]; then
 
     if [[ ! -d $project_path ]]; then
         echo "Project '$project_path' does not exist. What to do?"
-        echo "[A]ttach"
-        echo "[k]ill"
+        echo "[A]ttach tmux"
+        echo "[e]xit"
         local response
         read "response?Selection: "
         case $response in
             ""|A|a) tmux attach ;;
-            k) tmux kill-server ;;
-            *) echo "Unknown response. Killing..." tmux-kill-server ;;
+            e) exit 0 ;;
+            *) echo "Unknown response. Exit..." exit 0 ;;
         esac
         exit 0
     fi
 
-    tmux new-session -d -s $project_name -c $project_path
-    tmux rename-window -t $project_name run
-    tmux new-window -t "$project_name:2" -c $project_path -n edit
-    tmux new-window -t "$project_name:3" -c $project_path -n terminal
+    if ! tmux has-session -t $project_name; then
+        tmux new-session -d -s $project_name -c $project_path
+        tmux rename-window -t $project_name run
+        tmux new-window -t "$project_name:2" -c $project_path -n edit
+        tmux new-window -t "$project_name:3" -c $project_path -n terminal
+    fi
 fi
 
 tmux attach
