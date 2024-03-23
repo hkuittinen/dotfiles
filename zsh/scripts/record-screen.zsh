110a8bfce108e95df667446e2bd99904316a062f
Harri Kuittinen <harri.kuittinen@gmail.com>
2024-03-23 17:30:36 +0200

    Add screen recording

---
+++ zsh/scripts/record-screen.zsh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+if pgrep -x wf-recorder >/dev/null; then
+    notify-send "Saving..." -t 3000
+    killall -s SIGINT wf-recorder --wait
+    notify-send "Screen recorded." -t 3000
+else
+    area=$(slurp)
+    filename="$HOME/Videos/screen-recordings/$(date +"%Y-%m-%dT%H:%M:%S").mp4"
+    wf-recorder -g "$area" -f "$filename" &
+fi
+
+pkill -RTMIN+1 waybar
