9d7461bc8848054346167535a5982dfc5b3f234f
Harri Kuittinen <harri@vii.lu>
2024-05-22 14:06:57 +0300

    Add severity_sort true

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -86,6 +86,7 @@ return {
             })
 
             vim.diagnostic.config({
+                severity_sort = true,
                 virtual_text = false,
                 float = {
                     source = true,
