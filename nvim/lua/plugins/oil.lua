e92088030c33b42f260100bfd9be6846191bcb65
Harri Kuittinen <harri@vii.lu>
2024-05-23 17:11:41 +0300

    Add dir in the oil

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -3,6 +3,10 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
         require("oil").setup({
+            win_options = {
+                -- TODO: Change this to show home path as ~/. Or relative to cwd.
+                winbar = "%{v:lua.require('oil').get_current_dir()}",
+            },
             view_options = {
                 show_hidden = true,
             },
