98ad16bbe826ff9873e3714d66bb8e8eea649bea
Harri Kuittinen <harri.kuittinen@gmail.com>
2024-05-24 08:34:37 +0300

    Add oil winbar

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -2,15 +2,23 @@ return {
     "stevearc/oil.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-        require("oil").setup({
+        local oil = require("oil")
+
+        function OIL_GET_WINBAR()
+            local dir = oil.get_current_dir():gsub(vim.fn.getenv("HOME"), "~")
+            local cwd_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+            return dir .. " [cwd:" .. cwd_name .. "]"
+        end
+
+        oil.setup({
             win_options = {
-                -- TODO: Change this to show home path as ~/. Or relative to cwd.
-                winbar = "%{v:lua.require('oil').get_current_dir()}",
+                winbar = "%{v:lua.OIL_GET_WINBAR()}",
             },
             view_options = {
                 show_hidden = true,
             },
         })
+
         vim.keymap.set("n", "<leader>o", "<cmd>Oil<cr>")
     end,
 }
