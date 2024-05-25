f57d7de1ee845798028d29ed3a260b6cd30c10bc
Harri Kuittinen <harri.kuittinen@gmail.com>
2024-05-25 17:01:54 +0300

    Improve the tmux-start

--- zsh/scripts/tmux-start.zsh
+++ zsh/scripts/tmux-start.zsh
@@ -1,39 +1,37 @@
-#!/bin/zsh
-if [[ -v TMUX ]]; then
-    echo "You are already attached to a tmux session."
+#!/usr/bin/env zsh
+
+if [[ $# -eq 1 ]]; then
+    selected=$1
+else
+    selected=$(find ~/Projects -mindepth 1 -maxdepth 1 -type d | fzf)
+    is_project=true
+fi
+
+if [[ -z $selected ]]; then
     exit 0
 fi
 
-if ! tmux has-session; then
-    tmux new-session -d -s dotfiles -c ~/Projects/dotfiles
-    tmux new-session -d -s notes -c ~/Notes
-    tmux new-session -d -s terminal -c ~
+selected_name=$(basename "$selected" | tr . _)
+tmux_running=$(pgrep tmux)
+
+if [[ -z $tmux_running ]]; then
+    tmux new-session -ds dotfiles -c ~/Projects/dotfiles
+    tmux new-session -ds notes -c ~/Notes
+    tmux new-session -ds terminal -c ~
 fi
 
-if [[ $# -gt 0 ]]; then
-    local project_name=$1
-    local project_path=~/Projects/$project_name
+if ! tmux has-session -t=$selected_name 2> /dev/null; then
+    tmux new-session -ds $selected_name -c $selected
 
-    if [[ ! -d $project_path ]]; then
-        echo "Project '$project_path' does not exist. What to do?"
-        echo "[A]ttach tmux"
-        echo "[e]xit"
-        local response
-        read "response?Selection: "
-        case $response in
-            ""|A|a) tmux attach ;;
-            e) exit 0 ;;
-            *) echo "Unknown response. Exit..." exit 0 ;;
-        esac
-        exit 0
-    fi
-
-    if ! tmux has-session -t $project_name; then
-        tmux new-session -d -s $project_name -c $project_path
-        tmux rename-window -t $project_name run
-        tmux new-window -t "$project_name:2" -c $project_path -n edit
-        tmux new-window -t "$project_name:3" -c $project_path -n terminal
+    if [[ $is_project ]]; then
+        tmux rename-window -t $selected_name run
+        tmux new-window -t "$selected_name:2" -c $selected -n edit
+        tmux new-window -t "$selected_name:3" -c $selected -n terminal
     fi
 fi
 
-tmux attach
+if [[ -z $TMUX ]]; then
+    tmux attach
+else
+    tmux switch-client -t $selected_name
+fi
