7f7c5a160046d67133142b401aab61a50b7cfc79
Harri Kuittinen <harri.kuittinen@gmail.com>
2024-07-01 09:14:17 +0300

    Tweak nvim clojure

--- nvim/lua/plugins/clojure.lua
+++ nvim/lua/plugins/clojure.lua
@@ -49,14 +49,20 @@ return {
         init = function()
             -- Set configuration options here
             -- vim.g["conjure#debug"] = true
+            -- Disable the documentation mapping
+            vim.g["conjure#mapping#doc_word"] = false
+            -- Rebind it from K to <prefix>gk
+            vim.g["conjure#mapping#doc_word"] = "gk"
+            -- Reset it to the default unprefixed K (note the special table wrapped syntax)
+            -- vim.g["conjure#mapping#doc_word"] = { "K" }
         end,
     },
     -- Automatically balance parentheses.
-    {
-        "gpanders/nvim-parinfer",
-        ft = { "clojure" },
-        config = function()
-            vim.g.parinfer_force_balance = true
-        end,
-    },
+    -- {
+    --     "gpanders/nvim-parinfer",
+    --     ft = { "clojure" },
+    --     config = function()
+    --         vim.g.parinfer_force_balance = true
+    --     end,
+    -- },
 }
