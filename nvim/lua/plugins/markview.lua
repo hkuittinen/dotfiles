f3c72691404b47610f7ee7eec953009b89a18cf4
Harri Kuittinen <harri.kuittinen@gmail.com>
2025-02-07 22:12:03 +0200

    fix: Markview config

--- nvim/lua/plugins/markview.lua
+++ nvim/lua/plugins/markview.lua
@@ -1,42 +1,38 @@
 return {
     "OXY2DEV/markview.nvim",
-    lazy = false, -- Recommended
-    -- ft = "markdown" -- If you decide to lazy-load anyway
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter",
-        "nvim-tree/nvim-web-devicons",
-    },
+    lazy = false,
     config = function()
         require("markview").setup({
-            headings = {
-                shift_width = 0,
-                heading_1 = {
-                    style = "label",
-                    sign = "",
-                    padding_right = " ",
-                    icon = "# ",
+            markdown = {
+                headings = {
+                    shift_width = 0,
+                    heading_1 = {
+                        style = "label",
+                        sign = "",
+                        padding_right = " ",
+                        icon = "# ",
+                    },
+                    heading_2 = {
+                        style = "label",
+                        sign = "",
+                        padding_right = " ",
+                        icon = "## ",
+                    },
+                    heading_3 = {
+                        style = "label",
+                        sign = "",
+                        padding_right = " ",
+                        icon = "### ",
+                    },
                 },
-                heading_2 = {
-                    style = "label",
-                    sign = "",
-                    padding_right = " ",
-                    icon = "## ",
-                },
-                heading_3 = {
-                    style = "label",
-                    sign = "",
-                    padding_right = " ",
-                    icon = "### ",
+                list_items = {
+                    enable = false,
                 },
             },
-            list_items = {
-                enable = false,
-            },
-            checkboxes = {
-                enable = false,
-            },
-            code_blocks = {
-                sign = false,
+            markdown_inline = {
+                checkboxes = {
+                    enable = false,
+                },
             },
         })
     end,
