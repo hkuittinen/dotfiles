637f8c7e9c341c82917724208ce5bf8cc9acfbcd
Harri Kuittinen <harri.kuittinen@gmail.com>
2025-02-07 22:01:02 +0200

    Disable format on save

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -130,10 +130,10 @@ return {
         "stevearc/conform.nvim",
         config = function()
             require("conform").setup({
-                format_on_save = {
-                    timeout_ms = 500,
-                    lsp_fallback = false,
-                },
+                -- format_on_save = {
+                --     timeout_ms = 500,
+                --     lsp_fallback = false,
+                -- },
                 formatters_by_ft = {
                     lua = { "stylua" },
                     javascript = { "prettierd" },
@@ -142,9 +142,21 @@ return {
                     typescriptreact = { "prettierd" },
                     css = { "prettierd" },
                     html = { "prettierd" },
-                    clojure = { "cljstyle" },
+                    clojure = { "cljfmt" },
                 },
             })
+            vim.api.nvim_create_user_command("Format", function(args)
+                local range = nil
+                if args.count ~= -1 then
+                    local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+                    range = {
+                        start = { args.line1, 0 },
+                        ["end"] = { args.line2, end_line:len() },
+                    }
+                end
+                require("conform").format({ async = true, lsp_format = "never", range = range })
+            end, { range = true })
+            vim.keymap.set("n", "<leader>F", "<cmd>Format<CR>")
         end,
     },
 }
