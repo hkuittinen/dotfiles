1a014dedacc7b675e65407d32c9aca1776e0e6b2
Harri Kuittinen <harri.kuittinen@gmail.com>
2025-02-20 21:18:07 +0200

    Disable prettierd for now

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -136,12 +136,12 @@ return {
                 -- },
                 formatters_by_ft = {
                     lua = { "stylua" },
-                    javascript = { "prettierd" },
-                    javascriptreact = { "prettierd" },
-                    typescript = { "prettierd" },
-                    typescriptreact = { "prettierd" },
-                    css = { "prettierd" },
-                    html = { "prettierd" },
+                    -- javascript = { "prettierd" },
+                    -- javascriptreact = { "prettierd" },
+                    -- typescript = { "prettierd" },
+                    -- typescriptreact = { "prettierd" },
+                    -- css = { "prettierd" },
+                    -- html = { "prettierd" },
                     clojure = { "cljfmt" },
                 },
             })
