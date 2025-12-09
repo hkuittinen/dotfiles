34bc27ac997c6903bcbc361a4a051e3afb816557
Harri Kuittinen <harri.kuittinen@gmail.com>
2025-12-09 22:15:19 +0200

    Use fzf-lua for lsp picker

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -29,19 +29,29 @@ return {
                     local map = function(keys, func, desc)
                         vim.keymap.set("n", keys, func, { buffer = args.buf, desc = "LSP: " .. desc })
                     end
+                    local fzfLua = require("fzf-lua")
                     map("K", vim.lsp.buf.hover, "Hover Documentation")
                     map("<leader>i", vim.diagnostic.open_float, "Open Diagnostic Float")
-                    map("<leader>fr", vim.lsp.buf.references, "Find references")
-                    map("gd", vim.lsp.buf.definition, "Goto Definition")
+                    map("<leader>ca", fzfLua.lsp_code_actions, "Code Actions")
+                    map("<leader>fr", fzfLua.lsp_references, "Find references")
+                    map("<leader>fd", fzfLua.lsp_document_diagnostics, "Find document diagnostic")
+                    map("<leader>fD", fzfLua.lsp_workspace_diagnostics, "Find workspace diagnostic")
+                    map("gd", fzfLua.lsp_definitions, "Goto Definition")
                     map("gD", vim.lsp.buf.declaration, "Goto Declaration")
                     map("gs", vim.lsp.buf.signature_help, "Signature Documentation")
-                    map("<leader>ca", vim.lsp.buf.code_action, "Code Action")
                     map("<leader>rn", vim.lsp.buf.rename, "Rename all references")
-                    map("<leader>.", vim.lsp.completion.get, "Show completions")
 
                     local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
                     if client:supports_method("textDocument/completion") then
                         vim.cmd([[set completeopt+=menuone,noselect,popup]])
+
+                        -- Optional: trigger autocompletion on EVERY keypress. May be slow!
+                        local chars = {}
+                        for i = 32, 126 do
+                            table.insert(chars, string.char(i))
+                        end
+                        client.server_capabilities.completionProvider.triggerCharacters = chars
+
                         vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
                         vim.keymap.set("i", "<C-.>", function()
                             vim.lsp.completion.get()
